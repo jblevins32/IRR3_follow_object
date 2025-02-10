3ca48859024bc85f5b5880c656569d75769500a9
Jacob Blevins <104529767+jblevins32@users.noreply.github.com>
2025-02-09 20:12:27 -0500

    Delete build directory

--- build/chase_object/CMakeFiles/chase_object_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/chase_object_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/chase_object_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/find_object_py/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/jblevins32/IRR3/install/find_object_py'
