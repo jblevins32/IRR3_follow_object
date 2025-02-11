16b47d0733f4c22f50f2ae8b05501fa73413fb25
Jacob Blevins <104529767+jblevins32@users.noreply.github.com>
2025-02-10 20:01:11 -0500

    Delete build directory

--- build/get_object_range/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jblevins32/IRR3/src/get_object_range
-# Build directory: /home/jblevins32/IRR3/build/get_object_range
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
